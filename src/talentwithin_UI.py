8ac9a469462220d826c38cce89f779b3a9ad1a0e
jiahao_weng <jiahao_weng@kelaberetiv.sg>
2024-07-23 09:20:42 +0800

    rearrange UI elements on streamlit app

--- src/talentwithin_UI.py
+++ src/talentwithin_UI.py
@@ -80,70 +80,75 @@ if selected == "HR Module":
     st.image('../assets/images/process.png',width=1100)
     st.divider()
     st.markdown(" <style> div[class^='st-emotion-cache-13ln4jf ea3mdgi5'] { padding-left: 0rem; } </style> ", unsafe_allow_html=True)
+    st.subheader("HR Input")
     col1, col2 = st.columns([3,2])
 
     with col1:
-        st.subheader("HR Input")
-        st.write('Please review the details below and submit for internal transfer')
-        def text_field(label, columns=None, **input_params):
-            c1, c2 = st.columns([2,4])
+        st.markdown("######")
+        st.write('Please upload JD')
+        fl_upload = st.file_uploader("Upload File", type="pdf", label_visibility="collapsed")
 
-            with c1:
+        job_id_predefined = None
+        job_title_predefined = None
+        location_predefined = None
+
+        if fl_upload is not None:
+            upload_jd_endpoint = "http://localhost:8080/integrationservice/jd-upload"
+            files = [("file", (fl_upload.name, fl_upload.getvalue(), 'application/pdf'))]
+            response_jd_upload = requests.request("POST", upload_jd_endpoint, files=files)
+            jd_extracted_json = response_jd_upload.json()
+            job_id_predefined = jd_extracted_json['job_id']
+            job_title_predefined = jd_extracted_json['job_title']
+            location_predefined = jd_extracted_json['country']
+
+            st.markdown("######")
+            st.write('Please review/input the details below and submit for internal transfer')
+            def text_field(label, input_type="text", label_visibility="hidden", **input_params):
+                c1, c2 = st.columns([2,4])
+
+                with c1:
                     st.markdown("######")
                     st.write(label)
-    
-            input_params.setdefault("key", label)
+        
+                input_params.setdefault("key", label)
 
-            with c2:
-                    return c2.text_input("", **input_params)
+                with c2:
+                    if input_type=="text":
+                        return c2.text_input(label, label_visibility=label_visibility, **input_params)
+                    else:
+                        return c2.selectbox(label, label_visibility=label_visibility, **input_params)
 
 
-        job_id = text_field("Job ID")
-        job_title = text_field("Job Title")
-        corp_title = st.selectbox("Corporate Title", ("Analyst", "Associate", "Vice President", "Executive Director", "Managing Director"))
-        h_mgr = text_field("Hiring Manager")
-        loc = text_field("Location")
-        st.markdown("####")
-        st.write('Please upload JD')
-        fl_upload = st.file_uploader("Upload File", type="pdf")
+            job_id = text_field("Job ID", value=job_id_predefined)
+            job_title = text_field("Job Title", value=job_title_predefined)
+            corp_title = text_field("Corporate Title", 
+                                    input_type="dropdown",
+                                    options= ["Analyst", "Associate", "Vice President", "Executive Director", "Managing Director"])
+            h_mgr = text_field("Hiring Manager")
+            loc = text_field("Location", value=location_predefined)
+        
 
-    if fl_upload is not None:
-        upload_jd_endpoint = "http://localhost:8080/integrationservice/jd-upload"
-        files = [("file", (fl_upload.name, fl_upload.getvalue(), 'application/pdf'))]
-        response_jd_upload = requests.request("POST", upload_jd_endpoint, files=files)
-        jd_extracted_json = response_jd_upload.json()
     with col2:
-        st.subheader("Knowledge, Skills & Abilities (KSA)")
-        with st.container(height=360):
-            if fl_upload is not None:
-                # jd_extracted_json = {}
-                # jd_extracted_json['ksa'] = ["min 3 years of experience", "Database querying", "Oracle 19c", "MSSQL", "UNIX", "Windows", "Financial products", "FX", "Money Market", "Equity", "Derivatives", "Front-to-back operational workflows", "Different technologies", "Avaloq Certification"]
-                options = st.multiselect("Please select and edit the KSA if necessary",
-                                         jd_extracted_json['ksa'],
-                                         jd_extracted_json['ksa'])
+        if fl_upload is not None:
+            st.markdown("#### Knowledge, Skills & Abilities (KSA)")
+            with st.container(height=650):
+                if fl_upload is not None:
+                    # jd_extracted_json = {}
+                    # jd_extracted_json['ksa'] = ["min 3 years of experience", "Database querying", "Oracle 19c", "MSSQL", "UNIX", "Windows", "Financial products", "FX", "Money Market", "Equity", "Derivatives", "Front-to-back operational workflows", "Different technologies", "Avaloq Certification"]
+                    options = st.multiselect("Please select and edit the KSA if necessary",
+                                            jd_extracted_json['ksa'],
+                                            jd_extracted_json['ksa'])
 
-    def get_data():
-         return[]
-    
-    df_form = pd.DataFrame(columns=['Job ID','Job Title','Title','Manager'])
-    print(df_form)
-
-    if fl_upload is not None:
-        if st.button("Submit",type="primary"):
-            # d = {'Job ID': [job_id], 
-            #         'Job Title': [job_title],
-            #         'Title': [corp_title],
-            #         'Manager': [h_mgr]}
-            jd_extracted_json['ksa_reviewed'] = options
-            jd_extracted_json['job_id'] = job_id
-            jd_extracted_json['job_title'] = job_title
-            jd_extracted_json['corporate_title'] = corp_title
-            jd_extracted_json['country'] = loc
-            jd_extracted_json['hiring_manager'] = h_mgr
-            jd_submit_endpoint = "http://localhost:8080/integrationservice/jd-submit"
-            response_jd_submit = requests.request("POST", jd_submit_endpoint, data=jd_extracted_json)
-            st.write(jd_extracted_json)
-            print(jd_extracted_json)
+            if st.button("Submit",type="primary"):
+                jd_extracted_json['ksa_reviewed'] = options
+                jd_extracted_json['job_id'] = job_id
+                jd_extracted_json['job_title'] = job_title
+                jd_extracted_json['corporate_title'] = corp_title
+                jd_extracted_json['country'] = loc
+                jd_extracted_json['hiring_manager'] = h_mgr
+                jd_submit_endpoint = "http://localhost:8080/integrationservice/jd-submit"
+                response_jd_submit = requests.request("POST", jd_submit_endpoint, data=jd_extracted_json)
+                print(jd_extracted_json)
 
 
 if selected == "Talent Marketplace":
@@ -169,25 +174,27 @@ if selected == "Talent Marketplace":
         if "df" not in st.session_state:
             talent_results_endpoint = "http://localhost:8080/integrationservice/talent-results"
             response_talent_results = requests.get(talent_results_endpoint, params={"job-id": drp_jobtitle.split(" ")[0]}).json()
-
-            st.session_state.df = pd.DataFrame(
-                {
-                    "employee_id": response_talent_results['Serial Number'],
-                    "name": [str(m) + str(n) for m,n in zip(response_talent_results['First Name'], response_talent_results['Last Name'])],
-                    "location": response_talent_results['Country'],
-                    "title" : response_talent_results['Global Corporate Title'],
-                    "percentage": response_talent_results['score'],
-                }
-            )
+            talent_results_df = pd.DataFrame(response_talent_results)
+            talent_results_df['name'] = talent_results_df['First Name'] + talent_results_df['Last Name']
+            st.session_state.df = talent_results_df
+            # st.session_state.df = pd.DataFrame(
+            #     {
+            #         "employee_id": response_talent_results['Serial Number'],
+            #         "name": [str(m) + str(n) for m,n in zip(response_talent_results['First Name'], response_talent_results['Last Name'])],
+            #         "location": response_talent_results['Country'],
+            #         "title" : response_talent_results['Global Corporate Title'],
+            #         "percentage": response_talent_results['score'],
+            #     }
+            # )
 
         event = st.dataframe(
-            st.session_state.df,
+            st.session_state.df[["Serial Number", "name", "Country", "Global Corporate Title", "score"]],
             column_config={
-                "employee_id": "Employee ID",
+                "Serial Number": "Employee ID",
                 "name": "Candidate Name",
-                "location": "Location",
-                "title": "Corp Title",
-                "percentage": "Score",
+                "Country": "Location",
+                "Global Corporate Title": "Corp Title",
+                "score": "Score",
             },
             hide_index=True,
             key="data",
@@ -197,7 +204,7 @@ if selected == "Talent Marketplace":
         )
         
         selected_row = event.selection.rows
-        candidate_id  = st.session_state.df.iloc[selected_row]["employee_id"]
+        candidate_id  = st.session_state.df.iloc[selected_row]["Serial Number"]
     
     with col2:
         st.markdown(
@@ -229,6 +236,8 @@ if selected == "Talent Marketplace":
             
             st.write(st.session_state.df.iloc[selected_row]["name"])
             st.divider()
+            print("ksa type : ", type(st.session_state.df.iloc[selected_row]["ksa"]))
+            print("ksa list : ", st.session_state.df.iloc[selected_row]["ksa"])
             st.multiselect("",
                            st.session_state.df.iloc[selected_row]["ksa"],
                            st.session_state.df.iloc[selected_row]["ksa"], 
